b9d7757320b1cf8717a9ff5e3570e754d81c52fa
Frank Maes <franmaes.Maes@UGent.be>
2016-09-01 13:59:00 +0200

    added debian-82-x64 UGent acceptance tests
    Signed-off-by: Frank Maes <franmaes.Maes@UGent.be>

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -9,8 +9,100 @@ describe 'jchkmail class' do
       EOS
 
       # Run it twice and test for idempotency
-#      apply_manifest(pp, :catch_failures => true)
-#      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_failures => true)
     end
+
+
+    describe user('jchkmail') do
+      it { should exist }
+    end
+
+    describe group('jchkmail') do
+      it { should exist }
+    end
+
+    describe file('/etc/jchkmail/j-chkmail.cf') do
+      it { should be_file }
+      it { should be_mode 644 }
+      it { should be_owned_by 'root'}
+      it { should be_grouped_into 'root'}
+    end
+
+    describe file('/etc/jchkmail/j-ndc.cf') do
+      it { should be_file }
+      it { should be_mode 644 }
+      it { should be_owned_by 'root'}
+      it { should be_grouped_into 'root'}
+    end
+
+    describe file('/etc/jchkmail/cdb/j-bayes.db') do
+      it { should be_file }
+      it { should be_mode 644 }
+      it { should be_owned_by 'root'}
+      it { should be_grouped_into 'root'}
+    end
+
+    describe file('/etc/jchkmail/cdb/j-policy.db') do
+      it { should be_file }
+      it { should be_mode 644 }
+      it { should be_owned_by 'root'}
+      it { should be_grouped_into 'root'}
+    end
+
+    describe file('/etc/jchkmail/cdb/j-urlbl.db') do
+      it { should be_file }
+      it { should be_mode 644 }
+      it { should be_owned_by 'root'}
+      it { should be_grouped_into 'root'}
+    end
+
+    describe file('/etc/jchkmail/cdb/j-rcpt.db') do
+      it { should be_file }
+      it { should be_mode 644 }
+      it { should be_owned_by 'root'}
+      it { should be_grouped_into 'root'}
+    end
+
+    describe file('/var/lib/jchkmail') do
+      it { should be_directory }
+      it { should be_mode 755 }
+      it { should be_owned_by 'jchkmail'}
+      it { should be_grouped_into 'root'}
+    end
+
+    describe file('/etc/jchkmail/cdb') do
+      it { should be_directory }
+      it { should be_mode 755 }
+      it { should be_owned_by 'root'}
+      it { should be_grouped_into 'root'}
+    end
+
+
+    describe file('/var/spool/jchkmail') do
+      it { should be_directory }
+      it { should be_mode 755 }
+      it { should be_owned_by 'jchkmail'}
+      it { should be_grouped_into 'jchkmail'}
+    end
+
+    describe package('jchkmail') do
+      it { should be_installed }
+    end
+
+    describe package('make') do
+      it { should be_installed }
+    end
+
+    describe service('jchkmail') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+    describe service('jgreyd') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -17,6 +17,23 @@ RSpec.configure do |c|
     puppet_module_install(:source => proj_root, :module_name => 'jchkmail')
     hosts.each do |host|
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+
+      lsb_dcn = fact_on(host, 'lsbdistcodename')
+      commands_Debian = <<EOF
+if [ ! -e /etc/apt/sources.list.d/debs.list ]
+then
+  echo deb http://debs.ugent.be/debian #{lsb_dcn} main >  /etc/apt/sources.list.d/debs.list
+  apt-get update
+  apt-get install --allow-unauthenticated ugent-keyring
+  apt-get update
+fi
+EOF
+      if fact_on(host, 'osfamily') == 'Debian'
+        on host, commands_Debian
+      elsif fact_on(host, 'osfamily') == 'RedHat'
+        # fixme
+      end
+
     end
   end
 end
