9b2ce75ebb71e5bf6bfe14c44aad20b4bbdb9a66
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2016-09-06 14:18:36 +0200

    Lose v in version number

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class jchkmail::params
   $jndccf     = 'puppet:///modules/jchkmail/j-ndc.cf'
 
   ### j-chkmail parameters ###
-  $jchkmail_version = 'v2.3.1'
+  $jchkmail_version = '2.6.2'
 
   ### Override parameters can be defined by the user by passing a hash ###
   $config = {}
